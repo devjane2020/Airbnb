df93e515cfc0fe76e2173bb1ec46ff908ae238d1
Omotoyosi Adegbite <devjane2020@gmail.com>
2021-02-22 13:10:54 +0100

    Airbnb commit

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Footer.css"
+function Footer() {
+    return (
+        <div className="footer">
+            <p>
+            © 2021 Airbnb Clone! No right reserved - this is a demo!
+            </p>
+            <p>Privacy -  Terms - Sitemap - Company Details</p>
+        </div>
+    )
+}
+
+export default Footer
